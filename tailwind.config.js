2f116e2b9d212ae9d70e525577842b19127201bf
Rosie <rosie.bradshaw3@gmail.com>
2023-01-08 14:40:05 +0100

    testing to see if changing the directory of index.js and output.css to root helps show the published page correctly

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html","./dist/**/*{html,js}"],
+  content: ["./index.html","./dist/**/*{html,js}", "./src/**/*{html,js"],
   theme: {
     fontFamily:{
       'display':['R']
